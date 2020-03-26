d57732bafaf0f894a28ee677809862ade556a280
Vinyljam <hubertstrawa@gmail.com>
2020-03-26 12:59:06 +0100

    adding resource picker

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,34 @@
 import { TextStyle } from '@shopify/polaris';
 import { Page, EmptyState, Layout } from '@shopify/polaris';
+import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
 
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
-const Index = () => (
+class Index extends React.Component {
+  state = { open: false };
+  render() {
+
+    return (
     <Page>
+      <TitleBar
+      primaryAction={{
+        content: 'Select products',
+        onAction: () => this.setState({ open: true }),
+      }}
+      />
+      <ResourcePicker
+          resourceType="Product"
+          showVariants={false}
+          open={this.state.open}
+          onSelection={(resources) => this.handleSelection(resources)}
+          onCancel={() => this.setState({ open: false })}
+      />      
       <Layout>
         <EmptyState
           heading="Discount your products temporarily"
           action={{
             content: 'Select products',
-            onAction: () => console.log('clicked'),
+            onAction: () => this.setState({ open: true }),
           }}
           image={img}
         >
@@ -18,6 +36,15 @@ const Index = () => (
         </EmptyState>
       </Layout>
     </Page>
-);
+    );
+  }
 
-    export default Index
+  handleSelection = (resources) => {
+    const idsFromResources = resources.selection.map((product) => product.id);
+    this.setState({ open: false })
+    //console.log(resources)
+    console.log(idsFromResources)
+  };
+}
+
+export default Index
