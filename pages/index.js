11c56b99da2e7d95f0add0ed5d49179b8a5451e2
Vinyljam <hubertstrawa@gmail.com>
2020-03-29 23:53:17 +0200

    cleaning

--- pages/index.js
+++ pages/index.js
@@ -8,46 +8,6 @@ class Index extends React.Component {
   state = { open: false };
   render() {
 
-    function downloadData(contentType,data,filename){
-	 
-      let link=document.createElement("A");
-      link.setAttribute("href",encodeURI("data:"+contentType+","+data));
-      link.setAttribute("style","display:none");
-      link.setAttribute("download",filename);
-      document.body.appendChild(link); //needed for firefox
-      console.log(link.outerHTML);
-      link.click();
-      setTimeout(function(){
-          document.body.removeChild(link);
-      },1000);
-   }
-
-   function fromToXml(form){
-    let xmldata=['<?xml version="1.0"?>'];
-      xmldata.push("<form>");
-    let inputs=form.elements;
-    for(let i=0;i<inputs.length;i++){
-        let el=document.createElement("ELEMENT");
-      if (inputs[i].name){
-          el.setAttribute("name",inputs[i].name);
-        el.setAttribute("value",inputs[i].value);
-        xmldata.push(el.outerHTML);
-      }
-      
-    }
-    xmldata.push("</form>");
-    return xmldata.join("\n");
-}
-
-
- function download(frm) {
-  console.log('download')
-    let data=fromToXml(frm);
-  console.log(data);
-  
-  downloadData("text/xml",data,"export.xml");
-}
-
     return (
     <Page>
       <TitleBar
@@ -55,17 +15,14 @@ class Index extends React.Component {
         content: 'Select products',
         onAction: () => this.setState({ open: true }),
       }}
-      /> 
-
-      <form action="" method="post">
-        <label for="firstname">First Name:</label>  
-        <input type="text" name="firstname" id="firstname" />
-
-        <label for="lastName">Last Name:</label>
-        <input type="text" name="lastname" id="lastname"/>
-      <button type="button" onclick={download(this.form)} id="submitButton">Download</button>
-      </form>
-
+      />
+      <ResourcePicker
+          resourceType="Product"
+          showVariants={false}
+          open={this.state.open}
+          onSelection={(resources) => this.handleSelection(resources)}
+          onCancel={() => this.setState({ open: false })}
+      />      
       <Layout>
         <EmptyState
           heading="Discount your products temporarily"
@@ -88,7 +45,6 @@ class Index extends React.Component {
     //console.log(resources)
     console.log(idsFromResources)
   };
-
 }
 
 export default Index
